f73db74ef5e9e53b5cab6856ae95efe01b4f495e
cjawesomest <cjcalv@gmail.com>
2020-02-23 01:45:55 +0000

    New blockuwu

--- blocky/src/index.js
+++ blocky/src/index.js
@@ -1,37 +1,10 @@
-import * as blockstack from 'blockstack'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
 
-const appConfig = new blockstack.AppConfig()
-const userSession = new blockstack.UserSession({ appConfig: appConfig })
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById('signin-button').addEventListener('click', function (event) {
-      event.preventDefault()
-      userSession.redirectToSignIn()
-    })
-    document.getElementById('signout-button').addEventListener('click', function (event) {
-      event.preventDefault()
-      userSession.signUserOut(window.location.href)
-    })
-
-    function showProfile(profile) {
-      var person = new blockstack.Person(profile)
-      document.getElementById('heading-name').textContent = person.name() ? person.name() : "Nameless Person"
-      if (person.avatarUrl()) {
-        document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
-      }
-      else {
-        document.getElementById('avatar-image').setAttribute('src', './avatar-placeholder.png')
-      }
-      document.getElementById('section-1').style.display = 'none'
-      document.getElementById('section-2').style.display = 'block'
-    }
-
-    if (userSession.isUserSignedIn()) {
-      var profile = userSession.loadUserData().profile
-      showProfile(profile)
-    } else if (userSession.isSignInPending()) {
-      userSession.handlePendingSignIn().then(function (userData) {
-        window.location = window.location.origin
-      })
-    }
-  })
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'))
+registerServiceWorker()
